c0177edfaa789e94b7d88697ac55f5da6d41a3d1
takahumip7 <72800775+takahumip7@users.noreply.github.com>
2026-09-07 23:05:05 +0900

    @NotBlankのバリデーション付与 (#1)

--- src/main/java/com/example/todo/dto/TodoCreateRequest.java
+++ src/main/java/com/example/todo/dto/TodoCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.todo.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +8,6 @@ import lombok.Setter;
 @Setter
 public class TodoCreateRequest {
 
+  @NotBlank
   private String title;
 }
