2c5f6e0886affc3407b036594c52421aa2dfddb6
takahumip7 <72800775+takahumip7@users.noreply.github.com>
2026-09-10 22:53:00 +0900

    例外：Validation (#4)

--- src/main/java/com/example/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/todo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.example.todo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,5 +21,18 @@ public class GlobalExceptionHandler {
     ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(),
         request.getRequestURI());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationException(
+      MethodArgumentNotValidException ex,
+      HttpServletRequest request) {
+
+    FieldError error = ex.getBindingResult().getFieldErrors().get(0);
+
+    ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+        error.getDefaultMessage(),
+        request.getRequestURI());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
   }
 }
