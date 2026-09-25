d40aa193209a3073486ed14a09e7122139b3eda9
takahumip7 <72800775+takahumip7@users.noreply.github.com>
2026-09-25 23:46:09 +0900

    findTodoListのテストケース作成 (#10)

--- src/test/java/com/example/todo/service/TodoServiceTest.java
+++ src/test/java/com/example/todo/service/TodoServiceTest.java
@@ -3,6 +3,9 @@ package com.example.todo.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.todo.dto.TodoCreateRequest;
+import com.example.todo.dto.TodoResponse;
 import com.example.todo.entity.Todo;
 import com.example.todo.repository.TodoMapper;
 
@@ -45,7 +49,27 @@ public class TodoServiceTest {
   }
 
   // findTodoList 一覧を取得できる
+  @Test
+  void findTodoList_一覧を取得できること() {
+    // 準備
+    Todo todo = new Todo();
+    todo.setId(1L);
+    todo.setTitle("テスト");
+    todo.setCompleted(false);
 
+    List<Todo> todoList = List.of(todo);
+
+    when(todoMapper.findTodoList()).thenReturn(todoList);
+
+    // 実行
+    List<TodoResponse> actual = todoService.findTodoList();
+
+    // Assert（確認）
+    assertEquals(1, actual.size());
+    assertEquals(1L, actual.get(0).getId());
+    assertEquals("テスト", actual.get(0).getTitle());
+    assertFalse(actual.get(0).isCompleted());
+  }
   // deleteTodo 正常に削除できる
   // updateTodo 更新できるか
   // title と comletedを更新できる
